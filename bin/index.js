3b55cdfe7df5d599b7f0dd895a29613c0c5391e7
v_zqianchen <v_zqianchen@tencent.com>
2021-06-18 11:21:48 +0800

    A init

---
+++ bin/index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+
+function help() {
+  console.log(`Usage
+  cls-snippet --add`);
+}
+function main(conf = { path: '' }) {
+  let { path } = conf;
+
+  exec(`node ${path}/build/snippet.js`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+}
+
+const cmds = {
+  '--add': () => {
+    // note: npx 环境需要 process.cwd(), 找不到 process.env.INIT_CWD
+    let componentPath = `${process.cwd().replace(/\\/g, '/')}/node_modules/@tencent/weadmin-components-bizadmin`;
+    fs.access(componentPath, fs.constants.F_OK, err => {
+      if (!err) {
+        main({ path: componentPath });
+      } else {
+        console.log('wecomponent 不存在。请执行 tnpm i @tencent/weadmin-components-bizadmin');
+      }
+    });
+  },
+};
+const [, , cmd, ...args] = process.argv;
+cmds[cmd] ? cmds[cmd](...args) : help();
